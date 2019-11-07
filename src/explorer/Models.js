56161f865ca802fe329a258ed1a64d6fbb1571cd
xeolabs <lindsay.kay@xeolabs.com>
2019-11-07 22:41:41 +0100

    Fix loading dialog message

--- src/explorer/Models.js
+++ src/explorer/Models.js
@@ -57,7 +57,7 @@ class Models extends Controller {
         if (!modelInfo) {
             return;
         }
-        this.viewerUI.busyDialog.show(modelInfo.name);
+        this.viewerUI.busyDialog.show("Loading '" + modelInfo.name + "'");
         this.server.getModelMetadata(modelId,
             (json) => {
                 this.server.getModelGeometry(modelId,
