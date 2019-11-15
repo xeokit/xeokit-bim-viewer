d9a1d37ffb9e80d1dc4547ea417a7ba65b478788
xeolabs <lindsay.kay@xeolabs.com>
2019-11-15 11:23:27 +0100

    WebPack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const CircularDependencyPlugin = require('circular-dependency-plugin');
+
 
 module.exports = {
     entry: './index.js',
@@ -6,4 +8,19 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new CircularDependencyPlugin({
+            // exclude detection of files based on a RegExp
+            // exclude: /a\.js|node_modules/,
+            // include specific files based on a RegExp
+            //include: /dir/,
+            // add errors to webpack instead of warnings
+            failOnError: true,
+            // allow import cycles that include an asyncronous import,
+            // e.g. via import(/* webpackMode: "weak" */ './file.js')
+            allowAsyncCycles: false,
+            // set the current working directory for displaying module paths
+            cwd: process.cwd(),
+        })
+    ]
 };
