fb4eec8a8d648decf21f1de7b0b7a4266582e257
xeolabs <lindsay.kay@xeolabs.com>
2019-11-21 00:12:06 +0100

    Restore Webpack module rules for including CSS

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,38 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Adds CSS to the DOM by injecting a `<style>` tag
+                        loader: 'style-loader'
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader'
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    },
     resolve: {
         alias: {
             '/node_modules/@xeokit': "@xeokit"
