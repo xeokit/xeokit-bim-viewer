e892be57a61c6ba4590a7f211a3abd175b14b455
xeolabs <lindsay.kay@xeolabs.com>
2019-11-22 14:05:05 +0100

    Make index.html use Webpacked ./build/main.js

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 import {Server} from "./src/server/Server.js";
 import {ViewerUI} from "./src/ViewerUI.js";
 
-const server = new Server({
-    dataDir: "./data/"
-});
-
-const viewerUI = new ViewerUI(server, {
-    canvasElement: document.getElementById("myCanvas"), // WebGL canvas
-    explorerElement: document.getElementById("myExplorer"), // Left panel
-    toolbarElement: document.getElementById("myToolbar"), // Toolbar
-    navCubeCanvasElement: document.getElementById("myNavCubeCanvas"),
-    sectionPlanesOverviewCanvasElement: document.getElementById("mySectionPlanesOverviewCanvas")
-});
-
-viewerUI.on("queryPicked", (event) => {
-    const entity = event.entity; // Entity
-    const metaObject = event.metaObject; // MetaObject
-    alert(`Query result:\n\nObject ID = ${entity.id}\nIFC type = "${metaObject.type}"`);
-});
-
-// viewerUI.loadProject("duplex");
-viewerUI.loadProject("WestRiversideHospital");
-// viewerUI.loadProject("schependomlaan");
+export {ViewerUI};
+export {Server};
