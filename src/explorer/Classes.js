4e30c5f5ff56ea1478f9a62d99c0f9b45a41454b
xeolabs <lindsay.kay@xeolabs.com>
2020-01-06 15:29:07 +0100

    Use TreeViewPlugin in Classes explorer

--- src/explorer/Classes.js
+++ src/explorer/Classes.js
@@ -1,4 +1,5 @@
 import {Controller} from "../Controller.js";
+import {TreeViewPlugin} from "@xeokit/xeokit-sdk/src/plugins/TreeViewPlugin/TreeViewPlugin.js";
 
 class Classes extends Controller {
 
@@ -25,126 +26,31 @@ class Classes extends Controller {
         this._classesTabElement = cfg.classesTabElement;
         this._showAllClassesButtonElement = cfg.showAllClassesButtonElement;
         this._hideAllClassesButtonElement = cfg.hideAllClassesButtonElement;
-        this._classesElement = cfg.classesElement;
         this._classesTabButtonElement = this._classesTabElement.querySelector(".xeokit-tab-btn");
 
         if (!this._classesTabButtonElement) {
-            throw "Missing DOM element: ,xeokit-tab-btn";
+            throw "Missing DOM element: xeokit-tab-btn";
         }
 
-        this._muteCheckBoxEvents = false;
-        this._muteEntityEvents = false;
+        const classesElement = cfg.classesElement;
 
-        this._data = {};
-
-        this.viewer.scene.on("objectVisibility", (entity) => {
-            if (this._muteEntityEvents) {
-                return;
-            }
-            const metaObject = this.viewer.metaScene.metaObjects[entity.id];
-            if (!metaObject) {
-                return;
-            }
-            const type = metaObject.type;
-            const classData = this._data[type];
-            if (!classData) {
-                return;
-            }
-            this._muteCheckBoxEvents = true;
-            if (entity.visible) {
-                classData.numObjectsVisible++;
-                if (classData.numObjectsVisible === 1) {
-                    document.getElementById("" + type).checked = true;
-                }
-            } else {
-                classData.numObjectsVisible--;
-                if (classData.numObjectsVisible === 0) {
-                    document.getElementById("" + type).checked = false;
-                }
-            }
-            this._muteCheckBoxEvents = false;
+        this._treeView = new TreeViewPlugin(this.viewer, {
+            containerElement: classesElement,
+            hierarchy: "types",
+            autoAddModels: false
         });
 
-    }
-
-    _addModel(modelId) {
-        this._rebuild();
-    }
-
-    _removeModel(modelId) {
-        this._rebuild();
-    }
-
-    _rebuild(cfg) {
-        this._createData();
-        this._repaint();
-    }
-
-    _createData() {
-        var t0 = performance.now();
-        this._data = {};
-        const objectIds = this.viewer.scene.objectIds;
-        const metaObjects = this.viewer.metaScene.metaObjects;
-        const objects = this.viewer.scene.objects;
-        for (var i = 0, len = objectIds.length; i < len; i++) {
-            const objectId = objectIds[i];
-            const metaObject = metaObjects[objectId];
-            if (metaObject) {
-                const type = metaObject.type;
-                const entity = objects[objectId];
-                var classData = this._data[type];
-                if (!classData) {
-                    classData = {
-                        numObjectsVisible: 0,
-                        numObjects: 0
-                    };
-                    this._data[type] = classData;
-                }
-                if (entity.visible) {
-                    classData.numObjectsVisible++;
-                }
-                classData.numObjects++;
+        this._onModelLoaded = this.viewer.scene.on("modelLoaded", (modelId) =>{
+            if (this.viewer.metaScene.metaModels[modelId]) {
+                this._treeView.addModel(modelId);
             }
-        }
-        var t1 = performance.now();
-        // console.log("Classes._createData() " + (t1 - t0));
-    }
+        });
 
-    _repaint() {
-        const html = [];
-        for (var type in this._data) {
-            const classData = this._data[type];
-            html.push("<div class='xeokit-form-check'>");
-            html.push("<input id='");
-            html.push(type);
-            html.push("' type='checkbox' value=''");
-            if (classData.numObjectsVisible > 0) {
-                html.push(" checked ");
+        this._onModelUnloaded = this.viewer.scene.on("modelUnloaded", (modelId) => {
+            if (this.viewer.metaScene.metaModels[modelId]) {
+                this._treeView.removeModel(modelId);
             }
-            html.push(">");
-            html.push(type);
-            html.push("</div>");
-        }
-        this._classesElement.innerHTML = html.join("");
-        for (var type in this._data) {
-            const classData = this._data[type];
-            const checkBox = document.getElementById("" + type);
-            const objectIds = this.viewer.metaScene.getObjectIDsByType(type);
-            checkBox.addEventListener("click", () => {
-                if (this._muteCheckBoxEvents) {
-                    return;
-                }
-                const visible = checkBox.checked;
-                this._muteEntityEvents = true;
-                this.viewer.scene.setObjectsVisible(objectIds, visible);
-                if (visible) {
-                    classData.numObjectsVisible += objectIds.length;
-                } else {
-                    classData.numObjectsVisible -= objectIds.length;
-                }
-                this._muteEntityEvents = false;
-            });
-        }
+        });
     }
 
     setEnabled(enabled) {
@@ -158,34 +64,12 @@ class Classes extends Controller {
             this._hideAllClassesButtonElement.classList.remove("disabled");
         }
     }
-    muteEvents() {
-        this._muteCheckBoxEvents = true;
-        this._muteEntityEvents = true;
-    }
-
-    unmuteEvents() {
-        this._muteCheckBoxEvents = false;
-        this._muteEntityEvents = false;
-    }
-
-    selectAll() {
-        for (var type in this._data) {
-            const classData = this._data[type];
-            classData.numObjectsVisible = classData.numObjects;
-            document.getElementById("" + type).checked = true;
-        }
-    }
-
-    deselectAll() {
-        for (var type in this._data) {
-            const classData = this._data[type];
-            classData.numObjectsVisible = 0;
-            document.getElementById("" + type).checked = false;
-        }
-    }
 
     destroy() {
         super.destroy();
+        this._treeView.destroy();
+        this.viewer.scene.off(this._onModelLoaded);
+        this.viewer.scene.off(this._onModelUnloaded);
     }
 }
 
