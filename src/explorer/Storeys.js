33cf23616d2000ddfc4ce4f2534b047100d2f19c
xeolabs <lindsay.kay@xeolabs.com>
2020-01-23 12:27:29 +0100

    Fix model name in Storeys tree

--- src/explorer/Storeys.js
+++ src/explorer/Storeys.js
@@ -57,9 +57,13 @@ class Storeys extends Controller {
         });
 
         this._onModelLoaded = this.viewer.scene.on("modelLoaded", (modelId) =>{
-            if (this.viewer.metaScene.metaModels[modelId]) {
-                this._treeView.addModel(modelId);
+            const modelInfo = this.viewerUI.models.getModelInfo(modelId);
+            if (!modelInfo) {
+                return;
             }
+            this._treeView.addModel(modelId, {
+                rootName: modelInfo.name
+            });
         });
 
         this._onModelUnloaded = this.viewer.scene.on("modelUnloaded", (modelId) => {
