57e2391f3eb41f594314a288094aa4cf38e90be9
xeolabs <lindsay.kay@xeolabs.com>
2020-01-23 15:11:35 +0100

    Rebuild

--- dist/main.js
+++ dist/main.js
@@ -49503,6 +49503,35 @@ class XKTLoaderPlugin extends Plugin {
     }
 }
 
+/**
+ * @desc Default initial properties for {@link Entity}s loaded from models accompanied by metadata.
+ *
+ * When loading a model, a loader plugins such as {@link GLTFLoaderPlugin} and {@link BIMServerLoaderPlugin} create
+ * a tree of {@link Entity}s that represent the model. These loaders can optionally load metadata, to create
+ * a {@link MetaModel} corresponding to the root {@link Entity}, with a {@link MetaObject} corresponding to each
+ * object {@link Entity} within the tree.
+ *
+ * @type {{String:Object}}
+ */
+const IFCObjectDefaults$1 = {
+    IfcSpace: { // IfcSpace elements should be visible and pickable
+        visible: true,
+        pickable: true,
+        opacity: 0.2
+    },
+    IfcWindow: { // Some IFC models have opaque IfcWindow elements(!)
+        pickable: true,
+        opacity: 0.5
+    },
+    IfcOpeningElement: { // These tend to obscure windows
+        visible: false
+    },
+    IfcPlate: { // These tend to be windows(!)
+        colorize: [0.8470588235, 0.427450980392, 0, 0.5],
+        opacity: 0.5
+    }
+};
+
 const tempVec3$3 = math.vec3();
 
 class Models extends Controller {
@@ -49532,7 +49561,9 @@ class Models extends Controller {
             throw "Missing DOM element: ,xeokit-tab-btn";
         }
 
-        this._xktLoader = new XKTLoaderPlugin(this.viewer);
+        this._xktLoader = new XKTLoaderPlugin(this.viewer, {
+            objectDefaults: IFCObjectDefaults$1
+        });
         this._modelsInfo = {};
         this._numModelsLoaded = 0;
         this._projectId = null;
