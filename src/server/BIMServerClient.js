81bc871c8b125d625c16fde283b8a1c41256e7b9
xeolabs <lindsay.kay@xeolabs.com>
2020-07-29 15:34:28 +0200

    Initial BIMServerClient stub

---
+++ src/server/BIMServerClient.js
@@ -0,0 +1,91 @@
+
+/**
+ * Default BIMServerClient client which loads content for a {@link BIMViewer} from a BIMServer instance.
+ *
+ * A BIMViewer is instantiated with an instance of this class.
+ *
+ * To load content from an alternative source, instantiate BIMViewer with your own custom implementation of this class.
+ */
+class BIMServerClient {
+
+    /**
+     * Constructs a BIMServerClient.
+     *
+     * @param {*} [cfg] BIMServerClient configuration.
+     */
+    constructor(cfg = {}) {
+
+    }
+
+    /**
+     * Gets information on all available projects.
+     *
+     * @param {Function} done Callback through which the JSON result is returned.
+     * @param {Function} error Callback through which an error message is returned on error.
+     */
+    getProjects(done, error) {
+
+    }
+
+    /**
+     * Gets information for a project.
+     *
+     * @param {String} projectId ID of the project.
+     * @param {Function} done Callback through which the JSON result is returned.
+     * @param {Function} error Callback through which an error message is returned on error.
+     */
+    getProject(projectId, done, error) {
+
+    }
+
+    /**
+     * Gets metadata for a model within a project.
+     *
+     * @param {String} projectId ID of the project.
+     * @param {String} modelId ID of the model.
+     * @param {Function} done Callback through which the JSON result is returned.
+     * @param {Function} error Callback through which an error message is returned on error.
+     */
+    getMetadata(projectId, modelId, done, error) {
+
+    }
+
+    /**
+     * Gets geometry for a model within a project.
+     *
+     * @param {String} projectId ID of the project.
+     * @param {String} modelId ID of the model.
+     * @param {Function} done Callback through which the JSON result is returned.
+     * @param {Function} error Callback through which an error message is returned on error.
+     */
+    getGeometry(projectId, modelId, done, error) {
+
+    }
+
+    /**
+     * Gets metadata for an object within a model within a project.
+     *
+     * @param {String} projectId ID of the project.
+     * @param {String} modelId ID of the model.
+     * @param {String} objectId ID of the object.
+     * @param {Function} done Callback through which the JSON result is returned.
+     * @param {Function} error Callback through which an error message is returned on error.
+     */
+    getObjectInfo(projectId, modelId, objectId, done, error) {
+
+    }
+
+    /**
+     * Gets existing issues for a model within a project.
+     *
+     * @param {String} projectId ID of the project.
+     * @param {String} modelId ID of the model.
+     * @param {Function} done Callback through which the JSON result is returned.
+     * @param {Function} error Callback through which an error message is returned on error.
+     */
+    getIssues(projectId, modelId, done, error) {
+
+    }
+}
+
+export {BIMServerClient};
