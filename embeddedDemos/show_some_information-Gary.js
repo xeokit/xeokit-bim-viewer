4aa302b2f4587590557f91a03f5a856a335a39e0
Gary-Daveal1 <77924105+Gary-Daveal1@users.noreply.github.com>
2021-01-25 22:37:36 +0100

    Update show_some_information-Gary.js

--- embeddedDemos/show_some_information-Gary.js
+++ embeddedDemos/show_some_information-Gary.js
@@ -1,27 +1,17 @@
 "use strict";
 
-
 const ultrasonicSensor = "1bjALNe19BcO68YoPIpOu_";
 const depthSensor0 = "0nv5bie5r4AhhkrCbVL$Wn";
 const depthSensor1 = "25s0jSmSH4whopLALj9wxv";
 const depthSensor2 = "2oJVCaCyn2xPl5kA2E22TU";
 
-
-
-function init() {
-
-    const iframeBaseURL = "./../app/index.html?projectId=WaterLock";
-    let iframeElement = document.getElementById("embeddedViewer");
-    if (!iframeElement) {
-        throw "IFRAME not found";
-    }
-    iframeElement.src = iframeBaseURL;
+function show_some_information_init() {
 
     var iframe = document.getElementById('embeddedViewer');
 
 
     iframe.onload = function () {
-        var viewer = iframeElement.contentWindow.bimViewer.viewer;
+        var viewer = iframe.contentWindow.bimViewer.viewer;
         var id = null;
         var flag = true;
         viewer.cameraControl.on("hover", (e) => {
@@ -31,19 +21,16 @@ function init() {
             }
         });
 
-        viewer.scene.input.on("mouseup", e => {
+        viewer.scene.input.on("mousedown", e => {
 
             if (id === ultrasonicSensor || id === depthSensor0 || id === depthSensor1 || id === depthSensor2) {
                 id = 0;
                 flag = false;
-                alert("this is a sensor");
+                alert("this is a sensor",function (){
+                    flag = true;
+                });
             }
             console.log(id);
-        });
-
-        viewer.scene.input.on("mousedown", e => {
-
-            flag = true;
         });
 
         viewer.cameraControl.on("hoverOff", (e) => {
