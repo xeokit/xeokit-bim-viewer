da92f09006dba5d33beb98f97ee8ae34aadfe1a1
christine-lappe <christine.lappe@rwth-aachen.de>
2021-02-03 17:12:53 +0100

    Connected in master

---
+++ embeddedDemos/script.js
@@ -0,0 +1,31 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button =>{
+    button.addEventListener('click', () =>{
+        const modal = document.querySelector(button.dataset.modalTarget)
+         openModal(modal)
+    })
+
+})
+
+closeModalButtons.forEach(button =>{
+    button.addEventListener('click', () =>{
+        const modal = button.closest('.modal')
+         closeModal(modal)
+    })
+
+})
+
+function openModal(modal){
+    if (modal==null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal){
+    if (modal==null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
