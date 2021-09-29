ffe22a883d1bb6314c96a127ed0b8ccb67e9054a
xeolabs <lindsay.kay@xeolabs.com>
2021-09-29 13:42:46 +0200

    Inspector panel tweaks #93

--- src/inspector/PropertiesInspector.js
+++ src/inspector/PropertiesInspector.js
@@ -77,12 +77,15 @@ class PropertiesInspector extends Controller {
         if (!metaObject) {
             html.push(`<p class="subsubtitle">No object selected</p>`);
         } else {
-            html.push(`<p class="subsubtitle">"${metaObject.name}"</p>`);
+            html.push('<table class="xeokit-table">');
+            html.push(`<tr><td class="td1">Name:</td><td class="td2">"${metaObject.name}"</td></tr>`);
             if (metaObject.type) {
-                html.push(`<p class="subtitle">${metaObject.type}</p>`);
+                html.push(`<tr><td class="td1">Type:</td><td class="td2">"${metaObject.type}"</td></tr>`);
             }
+            html.push(`<tr><td class="td1">UUID:</td><td class="td2">"${metaObject.id}"</td></tr>`);
+            html.push('</table>');
             if (!propertySets || propertySets.length === 0) {
-                html.push(`<p class="subtitle">No properties sets found for this object.</p>`);
+                html.push(`<p class="subtitle xeokit-no-prop-set-warning">No properties sets found for this object.</p>`);
                 html.push(`</div>`);
             } else {
                 html.push(`</div>`);
