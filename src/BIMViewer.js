9bbc5a69133745bb48331c621b473238295b05a2
xeolabs <lindsay.kay@xeolabs.com>
2021-10-15 16:22:26 +0200

    Pass keyboardEventsElement into xeokit Viewer

--- src/BIMViewer.js
+++ src/BIMViewer.js
@@ -213,6 +213,7 @@ class BIMViewer extends Controller {
         const viewer = new Viewer({
             localeService: cfg.localeService,
             canvasElement: canvasElement,
+            keyboardEventsElement:keyboardEventsElement,
             transparent: false,
             backgroundColor: [1, 1, 1],
             backgroundColorFromAmbientLight: false,
