218d73926ffb2df51a985f25972267046237b173
lindsay <lindsay.kay@xeolabs.com>
2023-04-27 01:08:10 +0200

    Marquee selection tool refinements #132

--- src/toolbar/MarqueeTool.js
+++ src/toolbar/MarqueeTool.js
@@ -3,6 +3,10 @@ import {Frustum, frustumIntersectsAABB3, math, setFrustum} from "@xeokit/xeokit-
 
 const tempAABB3 = math.AABB3();
 
+const LEFT_TO_RIGHT = 0;
+const RIGHT_TO_LEFT = 1;
+
+
 /** @private */
 export class MarqueeTool extends Controller {
 
@@ -18,6 +22,7 @@ export class MarqueeTool extends Controller {
         this._marquee = math.AABB2();
         this._marqueeFrustum = new Frustum();
         this._marqueeFrustumProjMat = math.mat4();
+        this._marqueeDirection = false;
 
         const buttonElement = cfg.buttonElement;
 
@@ -32,7 +37,7 @@ export class MarqueeTool extends Controller {
         this.on("active", (active) => {
             if (active) {
                 buttonElement.classList.add("active");
-               const nop = this._objectsKdTree3.root; // Lazy-build the kd-tree
+                const nop = this._objectsKdTree3.root; // Lazy-build the kd-tree
             } else {
                 buttonElement.classList.remove("active");
             }
@@ -50,16 +55,14 @@ export class MarqueeTool extends Controller {
             this.setActive(false);
         });
 
-        const canvas = this.viewer.scene.canvas.canvas;
+        const scene = this.viewer.scene;
+        const canvas = scene.canvas.canvas;
 
         this._marqueeElement = document.createElement('div');
         document.body.appendChild(this._marqueeElement);
 
         const marqueeElement = this._marqueeElement;
         const marqueeStyle = marqueeElement.style;
-        marqueeStyle["border-radius"] = 3 + "px";
-        marqueeStyle.border = "solid 2px gray";
-        marqueeStyle.background = "rgba(200,200,200,0.3)";
         marqueeStyle.position = "absolute";
         marqueeStyle["z-index"] = "40000005";
         marqueeStyle.width = 8 + "px";
@@ -70,18 +73,18 @@ export class MarqueeTool extends Controller {
         marqueeStyle["box-shadow"] = "0 2px 5px 0 #182A3D;";
         marqueeStyle["opacity"] = 1.0;
         marqueeStyle["pointer-events"] = "none";
-
         let startX;
         let startY;
         let endX;
         var endY;
 
-        let canvasMarqueeMinX;
-        let canvasMarqueeMinY;
-        let canvasMarqueeMaxX;
-        let canvasMarqueeMaxY;
+        let canvasMarqueeStartX;
+        let canvasMarqueeStartY;
+        let canvasMarqueeEndX;
+        let canvasMarqueeEndY;
 
         let isDragging = false;
+        let mouseUpOffCanvas = false;
 
         let doubleClick = false;
         let timer = null;
@@ -90,28 +93,48 @@ export class MarqueeTool extends Controller {
             if (!this.getActive() || !this.getEnabled()) {
                 return;
             }
-            if (doubleClick) {
-                startX = e.pageX;
-                startY = e.pageY;
-                marqueeStyle.visibility = "visible";
-                marqueeStyle.left = startX + "px";
-                marqueeStyle.top = startY + "px";
-                marqueeStyle.width = "0px";
-                marqueeStyle.height = "0px";
-                marqueeStyle.display = "block";
-                canvasMarqueeMinX = e.offsetX;
-                canvasMarqueeMinY = e.offsetY;
-                isDragging = true;
-                this.viewer.cameraControl.pointerEnabled = false;
-                doubleClick = false;
-                clearTimeout(timer);
-            } else {
-                doubleClick = true;
-                timer = setTimeout(() => {
-                    doubleClick = false;
-                }, 1000); // set the time for double-click in milliseconds
-            }
+            scene.setObjectsSelected(scene.selectedObjectIds, false);
+            startX = e.pageX;
+            startY = e.pageY;
+            marqueeStyle.visibility = "visible";
+            marqueeStyle.left = startX + "px";
+            marqueeStyle.top = startY + "px";
+            marqueeStyle.width = "0px";
+            marqueeStyle.height = "0px";
+            marqueeStyle.display = "block";
+            canvasMarqueeStartX = e.offsetX;
+            canvasMarqueeStartY = e.offsetY;
+            isDragging = true;
+            this.viewer.cameraControl.pointerEnabled = false;
+            doubleClick = false;
+            clearTimeout(timer);
         });
+
+
+        canvas.addEventListener("mouseup", (e) => {
+            if (!this.getActive() || !this.getEnabled()) {
+                return;
+            }
+            if (!isDragging && !mouseUpOffCanvas) {
+                return
+            }
+            endX = e.pageX;
+            endY = e.pageY;
+            const width = Math.abs(endX - startX);
+            const height = Math.abs(endY - startY);
+            marqueeStyle.width = width + "px";
+            marqueeStyle.height = height + "px";
+            marqueeStyle.visibility = "hidden";
+            isDragging = false;
+            this.viewer.cameraControl.pointerEnabled = true;
+            if (mouseUpOffCanvas) {
+                mouseUpOffCanvas = false;
+            }
+            if (width > 3 || height > 3) {
+                this._marqueePick();
+            }
+
+        }); // Bubbling
 
         document.addEventListener("mouseup", (e) => {
             if (!this.getActive() || !this.getEnabled()) {
@@ -122,29 +145,9 @@ export class MarqueeTool extends Controller {
             }
             marqueeStyle.visibility = "hidden";
             isDragging = false;
+            mouseUpOffCanvas = true;
             this.viewer.cameraControl.pointerEnabled = true;
-            this._marqueePick();
         }, true); // Capturing
-
-        canvas.addEventListener("mouseup", (e) => {
-            if (!this.getActive() || !this.getEnabled()) {
-                return;
-            }
-            if (!isDragging) {
-                return
-            }
-            endX = e.pageX;
-            endY = e.pageY;
-            const width = endX - startX;
-            const height = endY - startY;
-            marqueeStyle.width = width + "px";
-            marqueeStyle.height = height + "px";
-            marqueeStyle.visibility = "hidden";
-            isDragging = false;
-            this.viewer.cameraControl.pointerEnabled = false;
-        }); // Bubbling
-
-        let timeout = -1;
 
         canvas.addEventListener("mousemove", (e) => {
             if (!this.getActive() || !this.getEnabled()) {
@@ -161,24 +164,25 @@ export class MarqueeTool extends Controller {
             marqueeStyle.height = Math.abs(height) + "px";
             marqueeStyle.left = Math.min(startX, x) + "px";
             marqueeStyle.top = Math.min(startY, y) + "px";
-            canvasMarqueeMaxX = e.offsetX;
-            canvasMarqueeMaxY = e.offsetY;
-            this._marquee[0] = Math.min(canvasMarqueeMinX, canvasMarqueeMaxX);
-            this._marquee[1] = Math.min(canvasMarqueeMinY, canvasMarqueeMaxY);
-            this._marquee[2] = Math.max(canvasMarqueeMinX, canvasMarqueeMaxX);
-            this._marquee[3] = Math.max(canvasMarqueeMinY, canvasMarqueeMaxY);
-            if (timeout > -1) {
-                clearTimeout(timeout);
+            canvasMarqueeEndX = e.offsetX;
+            canvasMarqueeEndY = e.offsetY;
+            const marqueeDirection = (canvasMarqueeStartX < canvasMarqueeEndX) ? LEFT_TO_RIGHT : RIGHT_TO_LEFT;
+            if (marqueeDirection !== this._marqueeDirection) {
+                marqueeStyle["background-image"] =
+                    marqueeDirection === LEFT_TO_RIGHT
+                        ? "url(\"data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='6' ry='6' stroke='%23333' stroke-width='4'/%3e%3c/svg%3e\")"
+                        : "url(\"data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='6' ry='6' stroke='%23333' stroke-width='4' stroke-dasharray='6%2c 14' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e\")";
+                this._marqueeDirection = marqueeDirection;
             }
-            timeout = setTimeout(() => {
-                this._marqueePick();
-            }, 1000);
-        });
+            this._marquee[0] = Math.min(canvasMarqueeStartX, canvasMarqueeEndX);
+            this._marquee[1] = Math.min(canvasMarqueeStartY, canvasMarqueeEndY);
+            this._marquee[2] = Math.max(canvasMarqueeStartX, canvasMarqueeEndX);
+            this._marquee[3] = Math.max(canvasMarqueeStartY, canvasMarqueeEndY);
 
-        document.addEventListener('dblclick', (event) => {
-            event.preventDefault();
+            console.log(this._marquee);
         });
     }
+
 
     _marqueePick() {
         this._rebuildFrustum();
@@ -190,28 +194,29 @@ export class MarqueeTool extends Controller {
             if (intersects === Frustum.OUTSIDE) {
                 return;
             }
-            //     if (intersects === Frustum.INTERSECT) {
             if (node.entities) {
                 const entities = node.entities;
                 for (let i = 0, len = entities.length; i < len; i++) {
-
                     const entity = entities[i];
                     const entityAABB = entity.aabb;
-                    const reducedEntityAABB = tempAABB3;
+                    // const reducedEntityAABB = tempAABB3;
+                    // reducedEntityAABB[0] = entityAABB[0] + ((entityAABB[3] - entityAABB[0]) / 5);
+                    // reducedEntityAABB[1] = entityAABB[1] + ((entityAABB[4] - entityAABB[1]) / 5);
+                    // reducedEntityAABB[2] = entityAABB[2] + ((entityAABB[5] - entityAABB[2]) / 5);
+                    // reducedEntityAABB[3] = entityAABB[3] - ((entityAABB[3] - entityAABB[0]) / 5);
+                    // reducedEntityAABB[4] = entityAABB[4] - ((entityAABB[4] - entityAABB[1]) / 5);
+                    // reducedEntityAABB[5] = entityAABB[5] - ((entityAABB[5] - entityAABB[2]) / 5);
 
-                    // reducedEntityAABB[0] = reducedEntityAABB[3] = (entityAABB[0] + entityAABB[3]) / 2;
-                    // reducedEntityAABB[1] = reducedEntityAABB[4] = (entityAABB[1] + entityAABB[4]) / 2;
-                    // reducedEntityAABB[2] = reducedEntityAABB[5] = (entityAABB[2] + entityAABB[5]) / 2;
-
-                    reducedEntityAABB[0] = entityAABB[0] + ((entityAABB[3] - entityAABB[0]) / 5);
-                    reducedEntityAABB[1] = entityAABB[1] + ((entityAABB[4] - entityAABB[1]) / 5);
-                    reducedEntityAABB[2] = entityAABB[2] + ((entityAABB[5] - entityAABB[2]) / 5);
-                    reducedEntityAABB[3] = entityAABB[3] - ((entityAABB[3] - entityAABB[0]) / 5);
-                    reducedEntityAABB[4] = entityAABB[4] - ((entityAABB[4] - entityAABB[1]) / 5);
-                    reducedEntityAABB[5] = entityAABB[5] - ((entityAABB[5] - entityAABB[2]) / 5);
-
-                    if (frustumIntersectsAABB3(this._marqueeFrustum, reducedEntityAABB) !== Frustum.OUTSIDE) {
-                        entities1.push(entity);
+                    if (this._marqueeDirection === LEFT_TO_RIGHT) { // Select entities that are completely inside marquee
+                        const intersection = frustumIntersectsAABB3(this._marqueeFrustum, entityAABB);
+                        if (intersection === Frustum.INSIDE) {
+                            entities1.push(entity);
+                        }
+                    } else {// Select entities that are partially inside marquee
+                        const intersection = frustumIntersectsAABB3(this._marqueeFrustum, entityAABB);
+                        if (intersection !== Frustum.OUTSIDE) {
+                            entities1.push(entity);
+                        }
                     }
                 }
             }
